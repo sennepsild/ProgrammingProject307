6f2c710544fada633764ddc3e7ffc409cfdff9dd
sennepsild <magnusmhp@gmail.com>
2016-11-29 13:13:45 +0100

    okay

---
+++ BoardGame/Assets/Scripts/JoinServer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JoinServer : MonoBehaviour {
+
+    Server theServer;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    void Giveipadress(string ipAdress)
+    {
+
+    }
+
+
+
+}
---
+++ BoardGame/Assets/Scripts/Server.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Text;
+public class Server  {
+
+    public static void Main()
+    {
+        Console.WriteLine("Starting echo server...");
+
+        int port = 1700;
+        TcpListener listener = new TcpListener(IPAddress.Any, port);
+        listener.Start();
+
+        TcpClient client = listener.AcceptTcpClient();
+        NetworkStream stream = client.GetStream();
+        StreamWriter writer = new StreamWriter(stream, Encoding.ASCII) { AutoFlush = true };
+        StreamReader reader = new StreamReader(stream, Encoding.ASCII);
+
+        while (true)
+        {
+            string inputLine = "";
+            while (inputLine != null)
+            {
+                inputLine = reader.ReadLine();
+                writer.WriteLine("Echoing string: " + inputLine);
+                Console.WriteLine("Echoing string: " + inputLine);
+            }
+            Console.WriteLine("Server saw disconnect from client.");
+        }
+    }
+}
