3a992c067d1676ff9276c597f7f5b82d74d766be
BreadStickBillie <billiesevelstedberthelsen@h269.aau1x.klient.sydhavn2.site.aau.dk>
2016-11-29 13:50:14 +0100

    board

---
+++ BoardGame/Assets/mouseDetect.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mouseDetect : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.name == "Player")
+            {
+                Debug.Log("This is a Player");
+            }
+            else
+            {
+                Debug.Log("This isn't a Player");
+            }
+        }
+
+    }
+}
