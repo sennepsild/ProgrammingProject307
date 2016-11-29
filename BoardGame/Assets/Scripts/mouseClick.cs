2299d50691d5301a3152b78659f9810c46829181
jorik990 <roofman990@gmail.com>
2016-11-29 13:38:48 +0100

    whatever doesnt matter

--- BoardGame/Assets/Scripts/mouseClick.cs
+++ BoardGame/Assets/Scripts/mouseClick.cs
@@ -1,13 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class script : MonoBehaviour {
+public class mouseClick : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+	//this is a mouse click script
 	// Update is called once per frame
 	void Update () {
 	
